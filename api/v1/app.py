805eae25b0fefe2b71b4de3bbfb7334a344a99b2
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-27 02:49:15 +0000

    Blank line added

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,7 @@ app.register_blueprint(app_views)
 # cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
