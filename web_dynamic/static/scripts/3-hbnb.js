53e7c84bb5daa2877d71de9e8654385e314f4843
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-27 03:17:23 +0000

    3-hbnb.js created Based on 2-hbnb.js

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,24 @@
+const $ = window.$;
+$(document).ready(function () {
+  const amenities = {};
+  $('input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    const amenichecked = Object.values(amenities);
+    if (amenichecked.length > 0) {
+      $('div.amenities > h4').text(Object.values(amenities).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+});
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  if (data.status === 'OK') {
+    $('DIV#api_status').addClass('available');
+  } else {
+    $('DIV#api_status').removeClass('available');
+  }
+});
