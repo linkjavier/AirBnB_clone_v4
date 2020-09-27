728ac11662f18f24de6b9e8ba652dc43d0089fde
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-27 02:32:52 +0000

    identation fixed on method .get

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,3 @@
-const { ok } = require("assert");
-
 const $ = window.$;
 $(document).ready(function () {
   const amenities = {};
@@ -11,16 +9,16 @@ $(document).ready(function () {
     }
     const amenichecked = Object.values(amenities);
     if (amenichecked.length > 0) {
-    $('div.amenities > h4').text(Object.values(amenities).join(', '));
+      $('div.amenities > h4').text(Object.values(amenities).join(', '));
     } else {
       $('div.amenities > h4').html('&nbsp;');
     }
   });
 });
-$.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
-    if (data.status === "OK") {
-      $('DIV#api_status').addClass('available')
-    } else {
-      $('DIV#api_status').removeClass('available') 
-    }
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  if (data.status === 'OK') {
+    $('DIV#api_status').addClass('available');
+  } else {
+    $('DIV#api_status').removeClass('available');
+  }
 });
