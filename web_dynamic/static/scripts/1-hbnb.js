6f2a24b55dfda220f8077e7b772cd737fcd91f5a
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-27 00:15:21 +0000

    Method slide added. two new variables created. 	* To join all amenities and 	* to to limited the message.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -9,7 +9,9 @@ $(document).ready(function () {
     }
     const amenichecked = Object.values(amenities);
     if (amenichecked.length > 0) {
-    $('div.amenities > h4').text(Object.values(amenities).join(', '));
+      const allamenichecked = amenichecked.join(', ');
+      const fewamenities = allamenichecked.slice(0, 30)
+      $('div.amenities > h4').text(fewamenities + '...');
     } else {
       $('div.amenities > h4').html('&nbsp;');
     }
